c784767eb3fe6b992146760cd483425d5d239518
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2013-07-01 21:07:38 +0300

    Fixed media and avatar path.

--- application/config/production/path.php
+++ application/config/production/path.php
@@ -11,7 +11,8 @@ return array(
 	|
 	*/
 	'media' => array(
-		'path' => '/home/gaian/domains/gaian.eu/public_html/files/media/',
+	    'path' => '/home/gaian/domains/gaian.eu/gaian/public/files/media/' //new path
+		//'path' => '/home/gaian/domains/gaian.eu/public_html/files/media/', //old path
 		'url' => 'http://www.gaian.eu/files/media/'
 	),
 
@@ -25,7 +26,8 @@ return array(
 	*/
 
 	'avatar' => array(
-		'path' => '/home/gaian/domains/gaian.eu/public_html/files/avatar/',
+		'path' => '/home/gaian/domains/gaian.eu/gaian/public/files/avatar/', //new path
+		//'path' => '/home/gaian/domains/gaian.eu/public_html/files/avatar/', //old path
 		'url' => 'http://www.gaian.eu/files/avatar/'
 	)
 
